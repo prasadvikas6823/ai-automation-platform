af85bd5ab0704943d5f50005ae340621181a5453
Vikas Prasad <prasadvikas6823@gmail.com>
2026-07-18 15:35:15 +0530

    feature/generate-test-case-from-ai-reponse

---
+++ src/infrastructure/parser/ResponseParser.ts
@@ -0,0 +1,33 @@
+import { TestCase } from "../../domain/models/TestCase";
+import { Step } from "../../domain/models/Step";
+
+interface AIResponse{
+    testCases: AITestCase[];
+}
+
+interface AITestCase {
+    title: string;
+    steps: string[];
+    expectedResult: string;
+}
+
+
+export class ResponseParser{
+
+    parse(response: string): TestCase[]{
+        const parsedResponse: AIResponse = JSON.parse(response);
+
+        return parsedResponse.testCases.map((testCase) => {
+
+            const steps = testCase.steps.map((step,index) =>{
+                return new Step(index+1,step);
+            });
+
+            return new TestCase(
+                testCase.title,
+                steps,
+                testCase.expectedResult
+            );
+        });
+    }
+}
