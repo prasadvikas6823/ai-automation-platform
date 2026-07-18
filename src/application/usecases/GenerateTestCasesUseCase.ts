f4bf8c395fd8af0d15300a6d2ad288e415e37ecd
Vikas Prasad <prasadvikas6823@gmail.com>
2026-07-18 16:14:52 +0530

    integrating the layer to generate test cases

--- src/application/usecases/GenerateTestCasesUseCase.ts
+++ src/application/usecases/GenerateTestCasesUseCase.ts
@@ -1,18 +1,22 @@
 import { RequirementReader } from "../../domain/contracts/RequirementReader";
 import { PromptBuilder } from "../PromptBuilder";
 import { LLMService } from "../../domain/contracts/LLMSerivice";
+import { ResponseParser } from "../../infrastructure/parser/ResponseParser";
+import { TestCase } from "../../domain/models/TestCase";
 
 export class GenerateTestCasesUseCase{
     constructor(
         private readonly requirementReader: RequirementReader,
         private readonly promtBuilder: PromptBuilder,
-        private readonly llmSerive: LLMService
+        private readonly llmSerive: LLMService,
+        private readonly responseParser: ResponseParser,
     ){}
 
-    async execute(): Promise<string>{
+    async execute(): Promise<TestCase[]>{
         const requirement = await this.requirementReader.read();
         const prompt = await this.promtBuilder.build(requirement);
         const response = await this.llmSerive.generate(prompt);
-        return response;
+        const testCases = this.responseParser.parse(response);
+        return testCases;
     }
 }
